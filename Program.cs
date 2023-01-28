d09b5c52463cb8e4169d659a7f5d76fd4d2d2ae3
Михаил Лазаренко <mihaillazarenko@192.168.0.9>
2023-01-29 02:58:40 +0300

    added function count of words

--- Program.cs
+++ Program.cs
@@ -1,30 +1,40 @@
-﻿Console.WriteLine("Введите количество слов");
-int n = Convert.ToInt32(Console.ReadLine());
-string[] matrix = new string[n];
-for (int i = 0; i < n; i++)
+﻿// Console.WriteLine("Введите количество слов");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string[] matrix = new string[n];
+// for (int i = 0; i < n; i++)
+// {
+// Console.WriteLine($"Введите слово {i+1}");
+// matrix[i] = Convert.ToString(Console.ReadLine());
+// }
+// int count = 0;
+// for (int i = 0; i < n; i++)
+// {
+// int length = matrix[i].Length;
+// if (length <= 3)
+// {
+// count++;
+// }
+// }
+// string[] newmatrix = new string[count];
+// for (int i = 0; i < n; i++)
+// {
+// int length = matrix[i].Length;
+// if (length <= 3)
+// {
+//     newmatrix[i] = matrix[i];
+// }    
+// }
+// for (int i = 0; i < count; i++)
+// {
+//         Console.WriteLine(newmatrix[i]);
+// }
+int EnterWords(string message) 
 {
-Console.WriteLine($"Введите слово {i+1}");
-matrix[i] = Convert.ToString(Console.ReadLine());
+    Console.WriteLine(message);
+    int count = Convert.ToInt32(Console.ReadLine());
+    return count;
 }
-int count = 0;
-for (int i = 0; i < n; i++)
-{
-int length = matrix[i].Length;
-if (length <= 3)
-{
-count++;
-}
-}
-string[] newmatrix = new string[count];
-for (int i = 0; i < n; i++)
-{
-int length = matrix[i].Length;
-if (length <= 3)
-{
-    newmatrix[i] = matrix[i];
-}    
-}
-for (int i = 0; i < count; i++)
-{
-        Console.WriteLine(newmatrix[i]);
-}
+
+
+int count = EnterWords("Введите количество слов");
+Console.WriteLine(count);
